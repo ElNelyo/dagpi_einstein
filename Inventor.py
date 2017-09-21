c38fddb8532328dc1dcaf237c6d8555bb1cd1d58
marin <marin.guerny@laposte.net>
2017-09-21 11:10:35 +0200

    Added_Inventor_class

---
+++ Inventor.py
@@ -0,0 +1,7 @@
+class Inventor:
+
+    # Init inventor with his name and victory points
+    def __init__(self, name, points):
+        self.name = name
+        self.points = points
+        self.sleep = False
