5afc2b24d77674e4b4418156df79bdf13f0a5528
charly <charly.strn@gmail.com>
2017-09-25 14:46:09 +0200

    Token on card

--- Gameboard.py
+++ Gameboard.py
@@ -5,9 +5,10 @@ import random
 
 class Gameboard:
     age = 0
-    cardsOnBoard    = []
-    playersOnBoard  = []
-    pocket          = []
+    cardsOnBoard            = []
+    playersOnBoard          = []
+    pocket                  = []
+    emplacementOnBoard      = []
 
     def __init__(self, tour, nbPlayer):
         self.tour = tour
@@ -19,7 +20,6 @@ class Gameboard:
     def distribute(self):
         cards = parse.Parse()
         cards = cards.getInvention()
-
         cpt = 0
 
         while cpt < self.nbCardToDistribute:
@@ -27,8 +27,20 @@ class Gameboard:
             if self.age == myCard.age:
                 print(myCard.name)
                 self.cardsOnBoard.append(myCard)
+
+                token1 = self.pocket[random.randint(0,len(self.pocket)-1)]
+                self.pocket.remove(token1)
+
+                token2 = self.pocket[random.randint(0, len(self.pocket) - 1)]
+                self.pocket.remove(token2)
+                temporarylist.append(myCard)
+                temporarylist.append(token1)
+                temporarylist.append(token2)
+                self.emplacementOnBoard.append(temporarylist)
+
                 cards.remove(myCard)
                 cpt += 1
+                
         return self.cardsOnBoard
 
     def wakeUp(self, player, coffee, inventor):
