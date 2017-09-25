69f1c9f57aa0b4bb6a95446836cbe1cd508fd237
charly <charly.strn@gmail.com>
2017-09-25 09:21:03 +0200

    Distribute card by age

--- Gameboard.py
+++ Gameboard.py
@@ -1,4 +1,43 @@
 # Create Gameboard class
+import parse
+import random
+
+
 class Gameboard:
-    def __init__(self, tour):
+    age = 0
+    cardsOnBoard = []
+    def __init__(self, tour, nbPlayer):
         self.tour = tour
+        self.age = 1
+        self.nbCardToDistribute = (nbPlayer +3)
+
+
+    def distribute(self,age):
+        cards = parse.Parse()
+        cards = cards.getInvention()
+        cpt = 0
+
+        while cpt < self.nbCardToDistribute:
+            myCard = cards[random.randint(0,len(cards))-1]
+            if(age==1 and myCard.age==1):
+                print(myCard.name)
+                self.cardsOnBoard.append(myCard)
+                cards.remove(myCard)
+                print(myCard.age)
+                cpt += 1
+            elif(age==2 and myCard.age==2):
+                print(myCard.name)
+                self.cardsOnBoard.append(myCard)
+                cards.remove(myCard)
+                print(myCard.age)
+                cpt += 1
+            elif(age==3 and myCard.age==3):
+                print(myCard.name)
+                print(myCard.age)
+                self.cardsOnBoard.append(myCard)
+                cards.remove(myCard)
+                cpt += 1
+
+        
+
+
