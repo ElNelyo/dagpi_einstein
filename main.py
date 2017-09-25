b9a564cca1d4002e405236b13b51d207b0479b6c
charly <charly.strn@gmail.com>
2017-09-25 14:52:10 +0200

    gameboard fix

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import parse
 import pygame
 import random
-import Gameboard 
+from Gameboard import Gameboard
 
 class Game():
     CIEL = 0, 200, 255
@@ -104,8 +104,8 @@ class Game():
 
         fondCard = pygame.image.load("backgroundCard.png").convert()
 
-        myGameboard = Gameboard.Gameboard(1,IAnumber)
-        myGameboard = myGameboard.distribute(1)
+        myGameboard = Gameboard(1,IAnumber)
+        myGameboard = myGameboard.distribute()
 
 
         self.loadPlayersBackgrounds(fenetre, fondCard, playerColor, IAnumber)
