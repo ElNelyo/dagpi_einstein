a58a6a881bc41af1fc0533681db840c6e1d57d50
Thomas Veloso <tveloso@IUT.BX1>
2017-09-25 09:11:29 +0200

    chooseTeam() Function in Player class, define inventors and team according to the color choose by player1

--- Player.py
+++ Player.py
@@ -1,8 +1,17 @@
+import parse
 class Player:
 
     def __init__(self, name, ia):
         self.Name = name
         self.IA = ia
 
+    def chooseTeam(colorChose):
+
+        player1 = Player.Player("player1", False)
+
+        for team in parse.Parse.myListTeam:
+            if colorChose == team.color:
+                player1.team = team
+                team.playerName = player1.Name
 
 
